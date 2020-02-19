158d92a881a57f1ce7d9b45aa66f2c7ab6ae9aa0
YuncaiMei <meiyuncai@hotmai.com>
2020-02-19 16:55:16 -0500

    test: clean code for `curvature_test.cpp` and `localization_test.cpp`.

--- losc/test/curvature_test.cpp
+++ losc/test/curvature_test.cpp
@@ -1,41 +1,30 @@
-#include <matrix/matrix.h>
+#include <cmath>
 #include <gtest/gtest.h>
-#include <vector>
+#include <matrix/matrix.h>
 #include <memory>
 #include <stdio.h>
 #include <string>
-#include <cmath>
+#include <vector>
 
 #include <losc/curvature.h>
 
-using std::vector;
-using std::shared_ptr;
 using matrix::Matrix;
+using std::shared_ptr;
 using std::string;
+using std::vector;
 
 using SharedMatrix = std::shared_ptr<Matrix>;
 
-struct CurvatureTest:public ::testing::TestWithParam<string> {
-    vector<shared_ptr<Matrix>> C_lo;
-    vector<shared_ptr<Matrix>> df_pmn;
-    vector<shared_ptr<Matrix>> df_Vpq_inverse;
-    vector<shared_ptr<Matrix>> grid_basis_value;
-    vector<shared_ptr<Matrix>> kappa_ref;
-    std::shared_ptr<vector<double>> grid_weight;
-
+struct CurvatureTest : public ::testing::TestWithParam<string> {
     string file_path;
     string dir_path;
-
     virtual void SetUp() override
     {
         file_path = __FILE__;
         dir_path = file_path.substr(0, file_path.rfind("/"));
     }
 
-    virtual void TearDown() override
-    {
-
-    }
+    virtual void TearDown() override {}
 };
 
 TEST_P(CurvatureTest, test)
@@ -45,35 +34,39 @@ TEST_P(CurvatureTest, test)
     const char *mol_str = mol.c_str();
     string lo_ref_path = dir_path + "/./data/" + mol + "/lo.txt";
     string df_pmn_path = dir_path + "/./data/" + mol + "/df_pmn.txt";
-    string df_Vpq_inv_path = dir_path + "/./data/" + mol + "/df_Vpq_inverse.txt";
-    string grid_basis_value_path = dir_path + "/./data/" + mol + "/grid_basis.txt";
+    string df_Vpq_inv_path =
+        dir_path + "/./data/" + mol + "/df_Vpq_inverse.txt";
+    string grid_basis_value_path =
+        dir_path + "/./data/" + mol + "/grid_basis.txt";
     string grid_weight_path = dir_path + "/./data/" + mol + "/grid_weight.txt";
     string kappa_ref_path = dir_path + "/./data/" + mol + "/kappa.txt";
-
-    C_lo = matrix::read_matrices_from_txt(lo_ref_path);
-    df_pmn = matrix::read_matrices_from_txt(df_pmn_path);
-    df_Vpq_inverse = matrix::read_matrices_from_txt(df_Vpq_inv_path);
-    grid_basis_value = matrix::read_matrices_from_txt(grid_basis_value_path);
-    kappa_ref = matrix::read_matrices_from_txt(kappa_ref_path);
-
+    auto C_lo = matrix::read_matrices_from_txt(lo_ref_path);
+    auto df_pmn = matrix::read_matrices_from_txt(df_pmn_path);
+    auto df_Vpq_inverse = matrix::read_matrices_from_txt(df_Vpq_inv_path);
+    auto grid_basis_value =
+        matrix::read_matrices_from_txt(grid_basis_value_path);
+    auto kappa_ref = matrix::read_matrices_from_txt(kappa_ref_path);
     auto grid_wt = matrix::read_matrices_from_txt(grid_weight_path);
     const size_t npts = grid_wt[0]->size();
-    grid_weight = std::make_shared<vector<double>>(npts);
+    auto grid_weight = std::make_shared<vector<double>>(npts);
     for (size_t i = 0; i < npts; ++i) {
         (*grid_weight)[i] = grid_wt[0]->data()[i];
     }
 
-    std::cout << "Mol: " << mol << std::endl;
-    std::cout << "data dir path: " << dir_path << std::endl;
-
     for (int is = 0; is < 2; is++) {
-        // compute curvature matrix.
-        losc::CurvatureV2 kappa_man(losc::B3LYP, C_lo[is], df_pmn[0], df_Vpq_inverse[0], grid_basis_value[0], grid_weight);
+        // Do calculation
+        losc::CurvatureV2 kappa_man(losc::B3LYP, C_lo[is], df_pmn[0],
+                                    df_Vpq_inverse[0], grid_basis_value[0],
+                                    grid_weight);
         auto kappa_calc = kappa_man.compute();
 
-        // verify the results.
+        // Test.
+        // True consition is that the calculated curvature matrix matches the
+        // reference to the 8-th digit.
         bool status = kappa_calc->is_equal_to(*kappa_ref[is], 1e-8);
         if (!status) {
+            std::cout << "Mol: " << mol << std::endl;
+            std::cout << "data dir path: " << dir_path << std::endl;
             printf("%s, Reference Kappa: spin=%d\n", mol_str, is);
             kappa_ref[is]->show_full();
             printf("%s, Calculated Kappa: spin=%d\n", mol_str, is);
@@ -83,10 +76,5 @@ TEST_P(CurvatureTest, test)
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    curvature_test,
-    CurvatureTest,
-    ::testing::Values(
-            "H2_1.0", "H2_10", "H2+_10",
-            "H2O")
-);
+INSTANTIATE_TEST_SUITE_P(curvature_test, CurvatureTest,
+                         ::testing::Values("H2_1.0", "H2_10", "H2+_10", "H2O"));
--- losc/test/localization_test.cpp
+++ losc/test/localization_test.cpp
@@ -1,22 +1,21 @@
-#include <matrix/matrix.h>
+#include <cmath>
 #include <gtest/gtest.h>
-#include <vector>
+#include <matrix/matrix.h>
 #include <memory>
 #include <stdio.h>
 #include <string>
-#include <cmath>
+#include <vector>
 
 #include <losc/localization.h>
 
-using std::vector;
-using std::shared_ptr;
 using matrix::Matrix;
+using std::shared_ptr;
 using std::string;
+using std::vector;
 
 using SharedMatrix = std::shared_ptr<Matrix>;
 
-
-bool is_permutation_matrix(Matrix &A, double thredshold=1e-10)
+bool is_permutation_matrix(Matrix &A, double thredshold = 1e-10)
 {
     if (!A.is_square()) {
         return false;
@@ -26,9 +25,8 @@ bool is_permutation_matrix(Matrix &A, double thredshold=1e-10)
         size_t n_one = 0;
         for (size_t j = 0; j < A.col(); ++j) {
             if (!(A(i, j) < thredshold) &&
-                !(std::fabs((A(i, j) - 1.0)) < thredshold)
-               ) {
-                   return false;
+                !(std::fabs((A(i, j) - 1.0)) < thredshold)) {
+                return false;
             } else if (std::fabs(A(i, j) - 1.0) < thredshold) {
                 n_one++;
                 if (n_one > 1) {
@@ -42,9 +40,8 @@ bool is_permutation_matrix(Matrix &A, double thredshold=1e-10)
         size_t n_one = 0;
         for (size_t i = 0; i < A.row(); ++i) {
             if (!(A(i, j) < thredshold) &&
-                !(std::fabs((A(i, j) - 1.0)) < thredshold)
-               ) {
-                   return false;
+                !(std::fabs((A(i, j) - 1.0)) < thredshold)) {
+                return false;
             } else if (std::fabs(A(i, j) - 1.0) < thredshold) {
                 n_one++;
                 if (n_one > 1) {
@@ -56,16 +53,7 @@ bool is_permutation_matrix(Matrix &A, double thredshold=1e-10)
     return true;
 }
 
-struct LocalizationTest:public ::testing::TestWithParam<string> {
-    size_t nlo = 4;
-    size_t nbasis = 4;
-
-    vector<shared_ptr<Matrix>> C_lo_basis;
-    vector<shared_ptr<Matrix>> H_ao;
-    vector<shared_ptr<Matrix>> D_ao;
-    vector<shared_ptr<Matrix>> C_lo_ref;
-    vector<shared_ptr<Matrix>> S_ao;
-
+struct LocalizationTest : public ::testing::TestWithParam<string> {
     string file_path;
     string dir_path;
 
@@ -75,14 +63,12 @@ struct LocalizationTest:public ::testing::TestWithParam<string> {
         dir_path = file_path.substr(0, file_path.rfind("/"));
     }
 
-    virtual void TearDown() override
-    {
-
-    }
+    virtual void TearDown() override {}
 };
 
 TEST_P(LocalizationTest, H2)
 {
+    // load data
     string mol = GetParam();
     const char *mol_str = mol.c_str();
     string lo_basis_path = dir_path + "/./data/" + mol + "/lo_basis.txt";
@@ -90,15 +76,6 @@ TEST_P(LocalizationTest, H2)
     string H_ao_path = dir_path + "/./data/" + mol + "/dfa_h.txt";
     string D_ao_path = dir_path + "/./data/" + mol + "/dipole_ao.txt";
     string S_ao_path = dir_path + "/./data/" + mol + "/ao_overlap.txt";
-
-    std::cout << "Mol: " << mol << std::endl;
-    std::cout << "data dir path: " << dir_path << std::endl;
-    std::cout << "LO basis coefficient: " << lo_basis_path << std::endl;
-    std::cout << "LO coefficient reference : " << lo_ref_path << std::endl;
-    std::cout << "DFA H under AO: " << H_ao_path << std::endl;
-    std::cout << "Dipole under AO: " << D_ao_path << std::endl;
-    std::cout << "AO overlap: " << S_ao_path << std::endl;
-
     auto C_lo_basis = matrix::read_matrices_from_txt(lo_basis_path);
     auto C_lo_ref = matrix::read_matrices_from_txt(lo_ref_path);
     auto H_ao = matrix::read_matrices_from_txt(H_ao_path);
@@ -106,27 +83,34 @@ TEST_P(LocalizationTest, H2)
     auto S_ao = matrix::read_matrices_from_txt(S_ao_path);
 
     for (int is = 0; is < 2; is++) {
-        // do localization.
+        // Do localization.
         losc::LoscLocalizerV2 localizer(C_lo_basis[is], H_ao[is], D_ao);
         localizer.set_random_permutation(false);
-        //localizer.set_print(losc::kPrintLevelNormal);
+        // localizer.set_print(losc::kPrintLevelNormal);
         auto C_lo_calc = localizer.compute();
 
-        // verify the correctness of calculated LO coefficient matrix.
+        // Test.
+        // True condition:
+        // 1. Directly compare the calculated LO coefficient matrix with the
+        // reference, to see if they can match up to 8-th digit.
+        // 2. Try to check if the calculated one is related to reference with
+        // a permutation transformation (negative sign is also okay, since it
+        // will change the physical observables).
         bool is_equal = C_lo_calc->is_equal_to(*C_lo_ref[is], 1e-8);
-        printf("%s, spin=%d: equal to the reference: %s\n", mol_str, is, is_equal ? "yes" : "no");
         SharedMatrix P;
         bool is_permutation = false;
-        if (! is_equal) {
+        if (!is_equal) {
+            size_t nlo = C_lo_basis[is]->row();
+            size_t nbasis = C_lo_basis[is]->col();
             SharedMatrix C1 = C_lo_calc;
             SharedMatrix C2 = C_lo_ref[is];
             SharedMatrix S = S_ao[0];
-            SharedMatrix C1SC1 = std::make_shared<Matrix> (nlo, nlo);
+            SharedMatrix C1SC1 = std::make_shared<Matrix>(nlo, nlo);
             matrix::mult_dgemm_ABAT(*C1, *S, *C1SC1);
             EXPECT_TRUE(C1SC1->is_identity(1e-8));
 
-            P = std::make_shared<Matrix> (nlo, nlo);
-            SharedMatrix C2S = std::make_shared<Matrix> (nlo, nbasis);
+            P = std::make_shared<Matrix>(nlo, nlo);
+            SharedMatrix C2S = std::make_shared<Matrix>(nlo, nbasis);
             matrix::mult_dgemm(1.0, *C2, "N", *S, "N", 0.0, *C2S);
             matrix::mult_dgemm(1.0, *C2S, "N", *C1, "T", 0.0, *P);
 
@@ -135,12 +119,24 @@ TEST_P(LocalizationTest, H2)
                 P->data()[i] = std::fabs(P->data()[i]);
             }
             is_permutation = is_permutation_matrix(*P, 1e-8);
-            printf("%s, spin=%d: relate to the reference with a permutation matrix: %s\n",
-                   mol_str, is, is_permutation ? "yes" : "no");
         }
+        EXPECT_TRUE(is_equal || is_permutation);
 
-        // Test failed. Print out matrices to check.
+        // If testing failed, print out matrices to check.
         if (!(is_equal || is_permutation)) {
+            printf("%s, spin=%d: equal to the reference: %s\n", mol_str, is,
+                   is_equal ? "yes" : "no");
+            printf("%s, spin=%d: relate to the reference with a permutation "
+                   "matrix: %s\n",
+                   mol_str, is, is_permutation ? "yes" : "no");
+            std::cout << "Mol: " << mol << std::endl;
+            std::cout << "data dir path: " << dir_path << std::endl;
+            std::cout << "LO basis coefficient: " << lo_basis_path << std::endl;
+            std::cout << "LO coefficient reference : " << lo_ref_path
+                      << std::endl;
+            std::cout << "DFA H under AO: " << H_ao_path << std::endl;
+            std::cout << "Dipole under AO: " << D_ao_path << std::endl;
+            std::cout << "AO overlap: " << S_ao_path << std::endl;
             printf("%s: AO overlap matrix:\n", mol_str);
             S_ao[0]->show_full();
             for (int xyz = 0; xyz < 3; xyz++) {
@@ -149,31 +145,17 @@ TEST_P(LocalizationTest, H2)
             }
             printf("%s: LO basis coefficient matrix: spin=%d\n", mol_str, is);
             C_lo_basis[is]->show_full();
-
             printf("%s: Hamiltonian under AO: spin=%d\n", mol_str, is);
             H_ao[is]->show_full();
-
-            printf("%s: LO coefficient matrix: reference. spin=%d\n", mol_str, is);
+            printf("%s: LO coefficient matrix: reference. spin=%d\n", mol_str,
+                   is);
             C_lo_ref[is]->show_full();
-
-            printf("%s: LO coefficient matrix: calculated. spin=%d\n", mol_str, is);
+            printf("%s: LO coefficient matrix: calculated. spin=%d\n", mol_str,
+                   is);
             C_lo_calc->show_full();
-
-            if (!is_equal) {
-                printf("%s: calculated LO coefficient matrix may relates to reference with a permutation matrix:\n", mol_str);
-                printf("The permutation matrix is:\n");
-                P->show_full();
-            }
         }
-
-        EXPECT_TRUE(is_equal || is_permutation);
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    Localizationtest,
-    LocalizationTest,
-    ::testing::Values(
-            "H2_1.0", "H2_10", "H2+_10",
-            "H2O")
-);
+INSTANTIATE_TEST_SUITE_P(Localizationtest, LocalizationTest,
+                         ::testing::Values("H2_1.0", "H2_10", "H2+_10", "H2O"));
