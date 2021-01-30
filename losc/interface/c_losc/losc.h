d1fc596522806d9b14523241cc8e900cad44d5e3
YuncaiMei <meiyuncai@hotmai.com>
2021-01-30 17:28:24 -0500

    interface/c_losc/localization: add C interface to localization.

--- losc/interface/c_losc/losc.h
+++ losc/interface/c_losc/losc.h
@@ -7,7 +7,8 @@
 #ifndef __LOSC_INTERFACE_C_LOSC_LOSC_H__
 #define __LOSC_INTERFACE_C_LOSC_LOSC_H__
 
-#include "curvature.h" // This brings the interface of curvature.
-#include "matrix.h"    // This brings the interface of matrix.
+#include "curvature.h"    // This brings the interface of curvature.
+#include "localization.h" // This brings the interface of localization.
+#include "matrix.h"       // This brings the interface of matrix.
 
 #endif
